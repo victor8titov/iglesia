296a7bfe143cd4d14b7409d214ae3cde182ab6cb
w8lt8r <victor8titov@ya.ru>
2018-08-01 15:17:53 +0300

    lesson 5

--- header.php
+++ header.php
@@ -14,7 +14,10 @@
 	<div class="wrapper cf">
 	
 	<div class="header_third search_section menu_buttom cf">
-		<div class="menu_icon"><i class="fa fa-bars" aria-hidden="true"></i></div>
+		<div class="menu_icon">
+			<i class="fa fa-bars" aria-hidden="true"></i>
+			<i class="fa fa-times" style= "display: none;" aria-hidden="true"></i>
+		</div>
 		<div class="search_box">
 		<?php echo get_search_form(); ?>
 		</div>
